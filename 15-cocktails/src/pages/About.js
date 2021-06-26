15a965534b2b35b0db19e3c08cd0e863d080f3fe
Vadim_Astapenko <o-maestpo@yandex.ru>
2021-06-27 00:06:31 +0300

    15-cocktails

---
+++ 15-cocktails/src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, quam enim quis, laudantium
+        minima, saepe aliquid illum totam quia molestias ipsa amet architecto praesentium cum
+        repellendus consequuntur ad eum corrupti molestiae itaque recusandae dignissimos culpa
+        voluptatem possimus. Corrupti, harum enim?
+      </p>
+    </section>
+  );
+};
+
+export default About;
